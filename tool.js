895cae3940db6708b80af85617df74ae29c202dc
Tzdy <49274160+Tzdy@users.noreply.github.com>
2019-06-25 12:55:22 +0800

    Update tool.js

--- tool.js
+++ tool.js
@@ -25,3 +25,29 @@ function ajax_get(url,...arrArgs) {    //url,是否异步,成功后执行函数
         xhr.open('GET',url,arrArgs[0]);
         xhr.send();
 }
+
+var Tsdy = window.Tsdy = function () { }
+    Tsdy.prototype.ajax = function (msg) {
+        if (msg.start ? msg.start() : true) {
+            var xhr = new XMLHttpRequest;
+            xhr.timeout = msg.timeout ? msg.timeout : 6000;
+            xhr.open(msg.method, msg.url, msg.async ? msg.async : true);
+            msg.loading ? msg.loading() : 0;
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    msg.end ? msg.end() : 0;
+                    msg.success(this);
+                }
+                if (xhr.readyState == 4 && xhr.status != 200) {
+                    msg.end ? msg.end() : 0;
+                    if (msg.failure)
+                        msg.failure(this);  
+                }
+            }
+            xhr.addEventListener('timeout', function () {
+                msg.ontimeout(this);
+            })
+            xhr.send();
+        }
+    }
+    window.ts = new Tsdy;
